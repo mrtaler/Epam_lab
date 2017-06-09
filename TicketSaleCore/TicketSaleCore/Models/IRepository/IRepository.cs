29eb80e5b4f9a93455c2c566fe0074d3f132b5d4
Taler <mrtaler@me.com>
2017-06-10 02:12:12 +0300

    up

---
+++ TicketSaleCore/TicketSaleCore/Models/IRepository/IRepository.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace TicketSaleCore.Models.IRepository
+{
+    public interface IRepository<T> : IDisposable
+          where T : class
+    {
+        IEnumerable<T> GetBookList(); // получение всех объектов
+        T GetBook(int id); // получение одного объекта по id
+        void Create(T item); // создание объекта
+        void Update(T item); // обновление объекта
+        void Delete(int id); // удаление объекта по id
+        void Save();  // сохранение изменений
+    }
+    /*
+    public interface IDbEntity
+    {
+        int Id { get; }
+    }
+
+    public abstract class RepositoryBase<T, DbT> : IRepository<T>
+        where T : Entity where DbT : class, IDbEntity, new()
+    {
+        protected readonly DbContext context = new DbContext();
+
+        public IQueryable<T> GetAll()
+        {
+            return GetTable().Select(GetConverter());
+        }
+
+        public bool Save(T entity)
+        {
+            DbT dbEntity;
+
+            if (entity.IsNew())
+            {
+                dbEntity = new DbT();
+            }
+            else
+            {
+                dbEntity = GetTable().Where(x => x.Id == entity.Id).SingleOrDefault();
+                if (dbEntity == null)
+                {
+                    return false;
+                }
+            }
+
+            UpdateEntry(dbEntity, entity);
+
+            if (entity.IsNew())
+            {
+                GetTable().InsertOnSubmit(dbEntity);
+            }
+
+            context.SubmitChanges();
+
+            entity.Id = dbEntity.Id;
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            var dbEntity = GetTable().Where(x => x.Id == id).SingleOrDefault();
+
+            if (dbEntity == null)
+            {
+                return false;
+            }
+
+            GetTable().DeleteOnSubmit(dbEntity);
+
+            context.SubmitChanges();
+            return true;
+        }
+
+        public bool Delete(T entity)
+        {
+            return Delete(entity.Id);
+        }
+
+        protected abstract Table<DbT> GetTable();
+        protected abstract Expression<Func<DbT, T>> GetConverter();
+        protected abstract void UpdateEntry(DbT dbEntity, T entity);
+    }*/
+    //public interface IDbContext
+    //{
+    //    IQueryable<T> Find<T>() where T : class;
+
+    //    void MarkAsAdded<T>(T entity) where T : class;
+
+    //    void MarkAsDeleted<T>(T entity) where T : class;
+
+    //    void MarkAsModified<T>(T entity) where T : class;
+
+    //    void Commit(bool withLogging);
+
+    //    //откатывает изменения во всех модифицированных объектах
+    //    void Rollback();
+
+    //    // включает или отключает отслеживание изменений объектов
+    //    void EnableTracking(bool isEnable);
+
+    //    EntityState GetEntityState<T>(T entity) where T : class;
+
+    //    void SetEntityState<T>(T entity, EntityState state) where T : class;
+
+    //    // возвращает объект содержащий список объектов с их состоянием
+    //    ChangeTracker GetChangeTracker();
+
+    //    DbEntityEntry GetDbEntry<T>(T entity) where T : class;
+    //}
+}
